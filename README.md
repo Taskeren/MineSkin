8101e7b12eba58982778314d9299cf75c2efc9e5
Taskeren <nitu2003@126.com>
2019-12-06 21:53:55 +0800

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # MineSkin
-To get the Minecraft players' skin image.
+The PHP program to get Minecraft players' skin.
+
+### How to use(Server)
+1. Star this repository.
+1. Download the index.php
+1. Place it to a PHP server.
+
+### How to use(Player)
+
+```
+https://api.taskeren.cn/MineSkin/?id={id}&format={format}
+```
+
+#### id
+The players' ID.
+
+#### format
+
+**(empty)**
+Return the url directly.
+```
+http://textures.minecraft.net/texture/d86bbfcda11dd58bf5576bb8e2c04b290893e690cf51584d08e63525caab2af7
+```
+
+**json**
+Return the json that contains `url`.
+```
+{"url":"http://textures.minecraft.net/texture/d86bbfcda11dd58bf5576bb8e2c04b290893e690cf51584d08e63525caab2af7"}
+```
+
+**image**
+Return the image file.
+![](http://textures.minecraft.net/texture/d86bbfcda11dd58bf5576bb8e2c04b290893e690cf51584d08e63525caab2af7)
---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+
+$userid = $_GET["id"];
+$format = $_GET["format"];
+
+if(empty($userid)) {
+    die();
+}
+
+$API_PROFILE_URL = "https://api.mojang.com/users/profiles/minecraft/";
+
+$profile = file_get_contents($API_PROFILE_URL.$userid);
+$profileJson = json_decode($profile, true);
+
+$sessionId = $profileJson["id"];
+
+$API_SESSION_URL = "https://sessionserver.mojang.com/session/minecraft/profile/";
+
+$session = @file_get_contents($API_SESSION_URL.$sessionId);
+$sessionJson = json_decode($session, true);
+
+if($session == null) {
+    header("HTTP/1.1 426 TooManyRequests");
+    die("426");
+}
+
+$textureB64 = $sessionJson["properties"][0]["value"];
+$textureRaw = base64_decode($textureB64);
+$textureJson = json_decode($textureRaw, true);
+
+$skin = $textureJson["textures"]["SKIN"]["url"];
+
+if($format == "image") {
+    header("Content-Type: image/png");
+    print(file_get_contents($skin, true));
+}
+else if($format == "json") {
+    header("Content-Type: application/json");
+    print("{\"url\":\"".$skin."\"}");
+}
+else {
+    print($skin);
+}
